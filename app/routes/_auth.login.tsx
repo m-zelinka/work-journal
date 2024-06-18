af7dc42e79d070fad8b51769287ecd1dd7969a5e
Marek Zelinka <mzelinka17@gmail.com>
2024-06-18 16:14:02 +0200

    add user login

---
+++ app/routes/_auth.login.tsx
@@ -0,0 +1,144 @@
+import { getFormProps, getInputProps, useForm } from "@conform-to/react";
+import { getZodConstraint, parseWithZod } from "@conform-to/zod";
+import {
+  json,
+  type ActionFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
+import { z } from "zod";
+import { ErrorList } from "~/components/forms";
+import { Logo } from "~/components/logo";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { createUserSession, verifyLogin } from "~/utils/auth.server";
+import { composeSafeRedirectUrl } from "~/utils/misc";
+
+const schema = z.object({
+  email: z
+    .string({ required_error: "Email is required" })
+    .trim()
+    .email("Email is invalid")
+    .min(3, "Email is too short")
+    // Users can type the email in any case, but we store it in lowercase
+    .transform((arg) => arg.toLowerCase()),
+  password: z
+    .string({ required_error: "Password is required" })
+    .trim()
+    .min(6, "Password is too short"),
+});
+
+export const meta: MetaFunction = () => {
+  return [{ title: "Login" }];
+};
+
+export async function action({ request }: ActionFunctionArgs) {
+  const url = new URL(request.url);
+  const redirectTo = composeSafeRedirectUrl(url.searchParams.get("redirectTo"));
+
+  const formData = await request.formData();
+
+  const submission = await parseWithZod(formData, {
+    schema: schema.transform(async (arg, ctx) => {
+      const user = await verifyLogin(arg.email, arg.password);
+
+      if (!user) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Invalid username or password",
+        });
+
+        return z.NEVER;
+      }
+
+      return { ...arg, user };
+    }),
+    async: true,
+  });
+
+  if (submission.status !== "success") {
+    return json(
+      { result: submission.reply({ hideFields: ["password"] }) },
+      { status: submission.status === "error" ? 400 : 200 },
+    );
+  }
+
+  const { user } = submission.value;
+
+  return await createUserSession({
+    request,
+    userId: user.id,
+    remember: true,
+    redirectTo,
+  });
+}
+
+export default function Component() {
+  const actionData = useActionData<typeof action>();
+
+  const [form, fields] = useForm({
+    constraint: getZodConstraint(schema),
+    lastResult: actionData?.result,
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+    onValidate: ({ formData }) => parseWithZod(formData, { schema }),
+  });
+
+  const [searchParams] = useSearchParams();
+
+  return (
+    <>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Logo className="mx-auto h-10 w-auto" />
+        <div className="text-center">
+          <h1 className="mt-10 text-3xl font-bold">Login</h1>
+          <p className="mt-2 text-balance text-muted-foreground">
+            Enter your email below to login to your account
+          </p>
+        </div>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <Form method="post" {...getFormProps(form)}>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor={fields.email.id}>Email</Label>
+              <Input
+                autoComplete="email"
+                placeholder="m@example.com"
+                {...getInputProps(fields.email, { type: "email" })}
+              />
+              <ErrorList
+                id={fields.email.errorId}
+                errors={fields.email.errors}
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor={fields.password.id}>Password</Label>
+              <Input
+                autoComplete="current-password"
+                {...getInputProps(fields.password, { type: "password" })}
+              />
+              <ErrorList
+                id={fields.password.errorId}
+                errors={fields.password.errors}
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+        </Form>
+      </div>
+      <p className="mt-10 text-center text-sm">
+        Don&apos;t have an account?{" "}
+        <Link
+          to={{ pathname: "/join", search: searchParams.toString() }}
+          className="underline"
+        >
+          Sign up
+        </Link>
+      </p>
+    </>
+  );
+}
