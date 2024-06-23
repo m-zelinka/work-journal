a33975368ea19f4741ef7b9955d6c3814755bb1f
Marek Zelinka <mzelinka17@gmail.com>
2024-06-23 21:57:56 +0200

    revert button style change

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { forwardRef, type ButtonHTMLAttributes } from 'react'
 import { cva, cx, type VariantProps } from '~/utils/misc'
 
 export const buttonVariants = cva({
-  base: 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  base: 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   variants: {
     variant: {
       default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
